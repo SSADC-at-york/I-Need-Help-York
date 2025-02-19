de600a923e3147457732dff1822416252be1514c
arnav malhotra <43398653+arnavmalhotra@users.noreply.github.com>
2025-02-19 01:32:56 -0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 // src/services/api.js
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://ineedhelpbackend.onrender.com';
 
 const getAuthHeader = () => {
   const token = localStorage.getItem('token');
