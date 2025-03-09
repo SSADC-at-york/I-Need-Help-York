7aef578ccd3684e651ae586c6f574e5b33f1e296
arnav malhotra <43398653+arnavmalhotra@users.noreply.github.com>
2025-03-08 21:13:35 -0500

    fixing CORS

--- backend/main.py
+++ backend/main.py
@@ -171,12 +171,13 @@ app = FastAPI()
 
 # Configure CORS
 origins = [
-    "https://ineedhelp.yorku.dev"
+    "https://ineedhelp.yorku.dev",
     "http://localhost:3000",
     "https://ineedhelp.onrender.com",
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
+    "https://yorku.dev",
 ]
 app.add_middleware(
     CORSMiddleware,
